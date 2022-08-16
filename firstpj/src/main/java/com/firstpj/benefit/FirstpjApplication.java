1d0f469c460222815789b6eb5651733f7eeeaba5
IM Jiyoung <hkkgjb468@gmail.com>
2022-08-16 14:43:02 +0900

    first commit

---
+++ firstpj/src/main/java/com/firstpj/benefit/FirstpjApplication.java
@@ -0,0 +1,13 @@
+package com.firstpj.benefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstpjApplication.class, args);
+	}
+
+}
---
+++ firstpj/src/test/java/com/firstpj/benefit/FirstpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstpj.benefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
